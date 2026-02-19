7479cc16c8de46597a98cf2b5c74efe595e5deea
MauRG15 <rojasgallegosmauricio@gmail.com>
2026-02-19 10:16:33 -0600

    Configuración de servicios de conexión y conversión de datos

---
+++ src/main/java/com/API/ApiComunicador.java
@@ -0,0 +1,29 @@
+package com.API;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiComunicador {
+    private String respuestaAPI;
+    public String comunicarConAPI(String direccion) {
+        //Conectar aplicacion con servicios
+        HttpClient client = HttpClient.newHttpClient();
+
+        //Solicitar datos de la API
+        URI direccionURI = URI.create(direccion);
+        HttpRequest request = HttpRequest.newBuilder().uri(direccionURI).build();
+
+        //Respuesta de la API
+        try {
+            HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+            respuestaAPI = response.body();
+            return respuestaAPI;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/com/API/IConvierteDatos.java
@@ -0,0 +1,5 @@
+package com.API;
+
+public interface IConvierteDatos {
+    <T> T obtenerDatos(String json, Class<T> clase);
+}
